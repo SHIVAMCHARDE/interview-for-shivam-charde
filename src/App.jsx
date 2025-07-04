2c7c31fcb92dc1bd95aeefec16a7715dec6af76e
SHIVAMCHARDE <83965173+SHIVAMCHARDE@users.noreply.github.com>
2025-07-04 23:44:41 +0530

    chore: initial CRA + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Dashboard from './pages/Dashboard';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+    <div>
+      <Dashboard/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+// src/pages/Dashboard.jsx
+export default function Dashboard() {
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+     <p>Dashboard</p>
+    </div>
+  );
+}
